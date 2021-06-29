1fde0aaf9924bb7e0a0c454ad44af5e86e9bd930
Rula Al-qasem <jaradatrula6@gmail.com>
2021-06-29 23:13:22 +0300

    done dk how

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -75,3 +75,63 @@ describe('Test ubdate /', () => {
     return supertest(server).put(`/food/9-9-9`).send(serialise(toSend)).expect(200);
   });
 });
+
+
+describe('api server', (req, res) => {
+
+  it('id unvalid', async () => {
+
+    const response = await mockreq.get(`/clothes`);
+
+    expect(response.status).toBe(200)
+  })
+
+  it('id unvalid', async () => {
+
+    const response = await mockreq.get(`/clothes/ttt`);
+
+    expect(response.status).toBe(500)
+  })
+
+
+})
+
+
+
+
+describe('Test post /', () => {
+  test('POST /example succeeds (200 OK)', () => {
+    const toSend = {
+      name: 'apple',
+    };
+
+    return supertest(server).post('/clothes').send(serialise(toSend)).expect(200);
+  });
+
+
+});
+describe('Test delete /', () => {
+
+  test('delete /example succeeds (204 OK) ', async () => {
+    const toSend = {
+      name: 'apple',
+      id: '9-9-9'
+    };
+    await mockreq.post('/clothes').send(serialise(toSend))
+
+    return supertest(server).delete(`/clothes/9-9-9`).send(serialise(toSend)).expect(204);
+  });
+});
+
+describe('Test ubdate /', () => {
+
+  test('put /example succeeds (200 OK) ', async () => {
+    const toSend = {
+      name: 'clothes',
+      id: '9-9-9'
+    };
+    await mockreq.post('/clothes').send(serialise(toSend))
+
+    return supertest(server).put(`/clothes/9-9-9`).send(serialise(toSend)).expect(200);
+  });
+});
